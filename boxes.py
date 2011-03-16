14f773a9c26188889905e0a34f57e795eab3de32
Morgan Redfield <redfieldm@gmail.com>
2011-03-16 16:17:06 -0700

    added option for top of box, command line control

--- boxes.py
+++ boxes.py
@@ -1,5 +1,6 @@
 from PIL import Image
-from math import sin, cos, pi, radians
+from math import sin, cos, pi, radians, ceil
+import sys
 
 import Image, ImageDraw
 
@@ -67,9 +68,11 @@ def make_dovetailed_side(length, teeth, stockwidth,start_high=True):
             
     return pts
     
-def make_box_side(length, width, teeth, stockwidth):
+def make_box_side(length, width, teeth, stockwidth, top_teeth = 0):
     if teeth%2!=0 or teeth==0:
         raise Exception( "number of teeth must be greater than 0 and divisible by 2" )
+    elif top_teeth%2!=0:
+    	raise Exception( "number of top teeth must be divisible by 2" )
         
     toothwidth = length/float(teeth)
     
@@ -81,12 +84,25 @@ def make_box_side(length, width, teeth, stockwidth):
             pts.append( move(pts[-1], -stockwidth, 0) )
         elif i!=teeth-1:
             pts.append( move(pts[-1], stockwidth, 0) )
-            
-    pts.append( move(pts[-1], width-stockwidth, 0) )
+    
+    if top_teeth != 0:
+    	top_toothwidth = (width-stockwidth)/float(top_teeth)
+    	for i in range(top_teeth):
+    		pts.append( move(pts[-1], top_toothwidth, 0) )
+    		
+    		if even(i):
+    			pts.append( move(pts[-1], 0, -stockwidth) )
+    		elif i != top_teeth-1:
+    			pts.append( move(pts[-1], 0, stockwidth) )
+    else:
+    	pts.append( move(pts[-1], width-stockwidth, 0) )
     
     for i in range(teeth):
         if i==teeth-1:
-            pts.append( move(pts[-1],0,-(toothwidth+stockwidth)) )
+        	if top_teeth != 0:
+        		pts.append( move(pts[-1],0,-toothwidth) )
+        	else:
+	            pts.append( move(pts[-1],0,-(toothwidth+stockwidth)) )
         else:
             pts.append( move(pts[-1],0,-toothwidth) )
         
@@ -95,9 +111,10 @@ def make_box_side(length, width, teeth, stockwidth):
         elif i!=teeth-1:
             pts.append( move(pts[-1], -stockwidth, 0) )
             
+    geom = [(int(round(x*40)),int(round(y*40))) for x,y in pts]    
+    print_design_image(geom, "side.png")
     return pts
     
-
 class LaserBox:
     def __init__(self, dx, dy, dz, stockwidth, dovetail_width):
         self.dx = dx
@@ -106,28 +123,45 @@ class LaserBox:
         self.stockwidth = stockwidth
         self.dovetail_width = dovetail_width
         
-    def design(self):
+    def design(self, top = False):
         geom=[]
         
         x_teeth = ((self.dx/(self.dovetail_width*2))+1)*2
         y_teeth = ((self.dy/(self.dovetail_width*2))+1)*2
         z_teeth = ((self.dz/(self.dovetail_width*2))+1)*2
             
-        dovetailed_sida_a = make_dovetailed_side(self.dx,x_teeth,self.stockwidth)
+        dovetailed_side_a = make_dovetailed_side(self.dx,x_teeth,self.stockwidth)
         dovetailed_side_b = make_dovetailed_side(self.dy,y_teeth,self.stockwidth)
 
-        geom.extend( dovetailed_sida_a,  )
+		#construct the bottom of the box
+        geom.extend( dovetailed_side_a )
         geom.extend( move_line(rotate_line(radians(-90),dovetailed_side_b[1:]),(self.dx,0) ) )
-        geom.extend( move_line(rotate_line(radians(-180),dovetailed_sida_a[1:]),(self.dx,-self.dy) ) )
+        geom.extend( move_line(rotate_line(radians(-180),dovetailed_side_a[1:]),(self.dx,-self.dy) ) )
         geom.extend( move_line(rotate_line(radians(-270),dovetailed_side_b[1:]),(0,-self.dy) ) )
-
-        box_side_a = make_box_side( self.dz, self.dx, z_teeth, self.stockwidth)
-        box_side_b = make_box_side( self.dz, self.dy, z_teeth, self.stockwidth)
+        
+        #construct the sides of the box
+        if top!=True:
+        	x_teeth = 0
+        	y_teeth = 0
+        box_side_a = make_box_side( self.dz, self.dx, z_teeth, self.stockwidth, x_teeth )
+        box_side_b = make_box_side( self.dz, self.dy, z_teeth, self.stockwidth, y_teeth )
+        
         geom.extend( box_side_a )
         geom.extend( move_line(rotate_line(radians(-90),box_side_b[1:]),(self.dx,0) ) )
         geom.extend( move_line(rotate_line(radians(-180),box_side_a[1:]),(self.dx,-self.dy) ) )
         geom.extend( move_line(rotate_line(radians(-270),box_side_b[1:]),(0,-self.dy) ) )
         
+        #construct the top of the box
+        if (top):
+        	geom.append( move(geom[-1], -self.stockwidth, 0) )
+        	dovetailed_top_a = make_dovetailed_side(self.dx,x_teeth,self.stockwidth)
+        	dovetailed_top_b = make_dovetailed_side(self.dy,y_teeth,self.stockwidth)
+        	
+        	#geom.extend( dovetailed_top_a )
+        	geom.extend( move_line(rotate_line(radians(-270),dovetailed_top_b[1:]),(0,self.dz-self.stockwidth) ) )
+        	geom.extend( move_line(rotate_line(radians(0),dovetailed_top_a[1:]),(0,self.dy+self.dz-self.stockwidth) ) )
+        	geom.extend( move_line(rotate_line(radians(-90),dovetailed_top_b[1:]),(self.dx,self.dy+self.dz-self.stockwidth) ) )
+
         #resize into PLT units
         geom = [(int(round(x*40)),int(round(y*40))) for x,y in geom]
             
@@ -139,12 +173,12 @@ class LaserBox:
 
 def print_design_image(design, filename="vector.png"):
 
-    geom = [(x,-y) for (x,y) in laserbox.design()]
+    geom = design #[(x,-y) for (x,y) in laserbox.design()]
             
     left,bottom,right,top = extent(geom)
 
-    width = right-left
-    height = top-bottom
+    width = (int)(right-left)
+    height = (int)(top-bottom)
             
     im = Image.new("RGB",(width+1,height+1))
 
@@ -169,16 +203,23 @@ def print_to_plt(design, filename="box.plt"):
     fp.close()
     
 if __name__=='__main__':
-    for i in range(5):
-        width=3+i*6
-        height=3+i*3
-        
-        print width, height
-        
-        laserbox = LaserBox( width, width, height, 3, 6 )
-
-        design = laserbox.design()
-        
-        print_design_image( design, filename="box%d.png"%i )
-        
-        print_to_plt(design, filename="box%d.plt"%i)
+	if len(sys.argv) != 7:
+		print len(sys.argv)
+		raise Exception( "Usage: python2.7 boxes.py width length depth stockwidth dovetail_width top" )
+	
+	values = [int(sys.argv[i]) for i in range(1,6)]
+	for i in range(0, 5):
+		if values[i] > 430 or values[i] < 3:
+			raise Exception( "input values must be between 3 and 430mm" )
+		#end if
+	#end for
+	
+	top = False
+	if sys.argv[6] == "True":
+		top = True
+	#end if
+	
+	laserbox = LaserBox(values[0], values[1], values[2], values[3], values[4])
+	design = laserbox.design(top)
+	print_design_image(design, "test_image.png")
+	print_to_plt(design, "boxes_output.plt")
